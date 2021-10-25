4ea22f79ac416f5ae9c18b8f084b78caf803c2f3
admin <admin@admins-MacBook-Pro.local>
2021-10-25 17:25:21 -0400

    terminado vista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frameworks';
+  contador = 1;
+  contador1 = 1;
+  contador2 = 1;
+
+  incrementar() {
+    this.contador++;
+  }
+  incrementar1() {
+    this.contador1++;
+  }
+  incrementar2() {
+    this.contador2++;
+  }
 }
